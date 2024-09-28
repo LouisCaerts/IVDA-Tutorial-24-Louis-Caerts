81e99641bdefd7968db462a90ddc4f56eebd507c
LouisC-1952673 <louis.caerts@student.uhasselt.be>
2024-09-28 09:59:38 +0200

    Up to "Basic Adjustments"

---
+++ example-project/hello-world/services/frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <h2>Louis Caerts - 23-724-693</h2>
+    </v-app-bar>
+    <v-main>
+      <ConfigurationPanel/>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+/* disable scrolling */
+html {
+  scrollbar-width: none; /* For Firefox */
+  -ms-overflow-style: none; /* For Internet Explorer and Edge */
+}
+html::-webkit-scrollbar {
+  width: 0px; /* For Chrome, Safari, and Opera */
+}
+</style>
+
+<script>
+import ConfigurationPanel from './components/ConfigurationPanel.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    ConfigurationPanel,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
